f3da16288e6755ff62ba59a731c3b14fa8b2d102
David.diaz <davidarield96@gmail.com>
2025-01-17 22:48:49 -0300

    Actualizacion en next.config.js para poder descargar el cv

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized : true
+    unoptimized: true
   },
-
   reactStrictMode: true,
+  static: {
+    public: 'public',
+  },
 }
 
 module.exports = nextConfig
